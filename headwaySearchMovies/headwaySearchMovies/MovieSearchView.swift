6eee24807e8538b1bcf587e42fe7e43552a8c4ef
Ivan <arturarutiunian@iMac.local>
2022-07-12 18:54:21 +0300

    project added

---
+++ headwaySearchMovies/headwaySearchMovies/MovieSearchView.swift
@@ -0,0 +1,46 @@
+//
+//  MovieSearchView.swift
+//  headwaySearchMovies
+//
+//  Created by Ivan Pestov on 12.07.2022.
+//
+
+import SwiftUI
+
+struct MovieSearchView: View {
+    
+    @ObservedObject var movieSearchState = MovieSearchState()
+    
+    var body: some View {
+        NavigationView {
+            List {
+                SearchBarView(placeholder: "Search movies", text: self.$movieSearchState.query)
+                    .listRowInsets(EdgeInsets(top: 0, leading: 8, bottom: 0, trailing: 8))
+                
+                LoadingView(isLoading: self.movieSearchState.isLoading, error: self.movieSearchState.error) {
+                    self.movieSearchState.search(query: self.movieSearchState.query)
+                }
+                
+                if self.movieSearchState.movies != nil {
+                    
+                    ForEach(self.movieSearchState.movies!) { movie in
+                        VStack(alignment: .leading) {
+                            Text(movie.title)
+                            
+                        }
+                    }
+                }
+            }
+            .onAppear {
+                self.movieSearchState.startObserve()
+            }
+            .navigationBarTitle("Search")
+        }
+    }
+}
+
+struct MovieSearchView_Previews: PreviewProvider {
+    static var previews: some View {
+        MovieSearchView()
+    }
+}
---
+++ headwaySearchMovies/headwaySearchMovies/headwaySearchMoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  headwaySearchMoviesApp.swift
+//  headwaySearchMovies
+//
+//  Created by Ivan Pestov on 12.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct headwaySearchMoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
